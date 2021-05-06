245c83bc9c96c883b360ac2f372ca679a00728c9
Pratt <aristopratt.work@gmail.com>
2021-05-06 18:20:50 -0500

    update user profile works

--- backend/base/urls/users_urls.py
+++ backend/base/urls/users_urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
       
 
     path('profile',views.getUserProfile, name="user-profile"),
+    path('profile/update/',views.updateProfile, name="user-profile-update"),
     path('',views.getUsers, name="users"),
     path('login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/',views.registerUser,name='register'),
