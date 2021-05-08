b14ab1964ada14712c449d4b67dd8e886de97a68
Pratt <aristopratt.work@gmail.com>
2021-05-08 13:43:24 -0500

    Paypal integration and place orders complete

--- backend/base/urls/orders_urls.py
+++ backend/base/urls/orders_urls.py
@@ -3,5 +3,6 @@ from base.views import order_views as views
 
 urlpatterns = [
     path('add/', views.addOrderItems, name='orders-add'),
-    path('<str:pk>/', views.getOrderById, name='order-get')
+    path('<str:pk>/', views.getOrderById, name='order-get'),
+    path('<str:pk>/pay', views.updateOrderToPaid, name='pay')
 ]
