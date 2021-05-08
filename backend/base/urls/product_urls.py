99a48fc147673f50e2b274e4a12ad4079584842f
Pratt <aristopratt.work@gmail.com>
2021-05-08 18:59:24 -0500

    update product admin

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -3,8 +3,11 @@ from base.views import product_views as views
 
 urlpatterns = [
  
-    path('',views.getProducts, name="products"),  
-    path('<str:pk>',views.getProduct, name="product"),
+    path('',views.getProducts, name="products"), 
 
+    path('create/',views.createProduct, name="create"),     
+    path('<str:pk>',views.getProduct, name="product"),
+    
+    path('update/<str:pk>/',views.updateProduct, name="product-update"),
     path('delete/<str:pk>/',views.deleteProduct, name="delete"),
 ]
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import ProductListScreen from "./screens/ProductListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
@@ -38,6 +39,7 @@ function App() {
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
 
           <Route path="/admin/productlist" component={ProductListScreen} />
+          <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
         </Container>
       </main>
       <Footer></Footer>
