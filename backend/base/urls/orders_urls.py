8d7e8e17f36b95f5f7e4ec99da4dc0f189a67ce7
Pratt <aristopratt.work@gmail.com>
2021-05-08 19:56:51 -0500

    Admin panel functional

--- backend/base/urls/orders_urls.py
+++ backend/base/urls/orders_urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('',views.getOrders,name='orders'),
     path('add/', views.addOrderItems, name='orders-add'),
     path('myorders/',views.getMyOrders,name='myorders'),
+    path('<str:pk>/deliver/', views.updateOrderToDelivered, name='order-deliver'),
     path('<str:pk>/', views.getOrderById, name='order-get'),
     path('<str:pk>/pay', views.updateOrderToPaid, name='pay')
 ]
